6af70fa1a8bb2db5b63c12e536e16b6446c9d9f8
Erin <hoople.erin@gmail.com>
2014-04-06 13:15:26 -0500

    added top sidebar

--- product-page.php
+++ product-page.php
@@ -10,9 +10,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<!-- Product Sub Menu -->
-			<?php wp_nav_menu( array( 'theme_location' => 'product-sub-menu' ) ); ?>
-           
+			<?php if ( ! dynamic_sidebar( 'top-sidebar' ) ) : ?>
+			<?php endif; ?>
+			
 			<!-- Product Section -->
 			<div class="product-section">
 				<!-- Featured Product Image -->
@@ -30,31 +30,29 @@ get_header(); ?>
 
 				<!-- Specific Product Description / Specs -->
 	            <p class="product-details-intro"><?php the_field('product_details_intro'); ?></p>
-	           
-	            <?php
-	            	// check if the repeater field has rows of data
-					if( have_rows('product_details') ):
-			 
-			 		// loop through the rows of data
-			    	while ( have_rows('product_details') ) : the_row();
+	            <ul class="product-details">
+		            <?php
+		            	// check if the repeater field has rows of data
+						if( have_rows('product_details') ):
+				 
+				 		// loop through the rows of data
+				    	while ( have_rows('product_details') ) : the_row();
 
-			        // display a sub field value ?>
+				        // display a sub field value ?>
 
-			        <ul class="product-details">
-			        	<li><?php the_sub_field('list_item'); ?></li>
-			        </ul>
+				        	<li><?php the_sub_field('list_item'); ?></li>
 			     	
-		     	<?php
+		     		<?php
 		 
-		    		endwhile;
+		    			endwhile;
 		 
-					else :
+						else :
 		 
-		    		// no rows found
+		    			// no rows found
 		 
-					endif;
-		 
-				?>
+						endif;
+					?>
+				</ul>
 
 	            <!-- Call Out Box -->
 	            <p class="call-out-box"><?php the_field('call_out_box'); ?></p>
